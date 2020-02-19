bef49cdd0c9bc10ac0284aed4ee2709dd49f5964
apriliciaaura <60381239+apriliciaaura@users.noreply.github.com>
2020-02-19 15:39:44 +0700

    Add files via upload

---
+++ storage/framework/views/5155be9825f2570a12c010278c5b290086ed1362.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+	Dashboard
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\tugas4\resources\views/dashboards/index.blade.php ENDPATH**/ ?>
